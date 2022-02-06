db3fc59348706eb05646cde8943741a1e479693c
ismailAbdirahman <ismail960i960@gmail.com>
2022-02-06 23:37:49 +0300

    source of images website

--- lib/screens/settings/settings.dart
+++ lib/screens/settings/settings.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:url_launcher/url_launcher.dart';
 import 'package:wiifd/app_state/settings_state.dart';
 import 'package:wiifd/data_model/profile_settings.dart';
 import 'package:wiifd/data_source/supabase_db.dart';
@@ -47,35 +48,55 @@ class SettingsScreenState extends ConsumerState<SettingsScreen> {
         ),
         resizeToAvoidBottomInset: false,
         body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.all(18.0),
-              child: InkWell(
-                onTap: () {},
-                child: TextField(
-                  controller: nameController,
-                  decoration: InputDecoration(
-                      border: InputBorder.none, suffixIcon: Icon(Icons.edit)),
+            Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: InkWell(
+                    onTap: () {},
+                    child: TextField(
+                      controller: nameController,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          suffixIcon: Icon(Icons.edit)),
+                    ),
+                  ),
                 ),
-              ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        primary: AppColor().primaryColor),
+                    onPressed: () async {
+                      final model = ref.read(settingsProvider.notifier);
+                      final success =
+                          await model.updateName(nameController!.value.text);
+                      if (success) {
+                        FocusManager.instance.primaryFocus?.unfocus();
+                      } else {
+                        print("not saved");
+                      }
+                    },
+                    child: Text('Update')),
+              ],
             ),
-            ElevatedButton(
-                style:
-                    ElevatedButton.styleFrom(primary: AppColor().primaryColor),
-                onPressed: () async {
-                  final model = ref.read(settingsProvider.notifier);
-                  final success =
-                      await model.updateName(nameController!.value.text);
-                  if (success) {
-                    FocusManager.instance.primaryFocus?.unfocus();
-                  } else {
-                    print("not saved");
-                  }
-                },
-                child: Text('Update')),
+            Padding(
+              padding: const EdgeInsets.only(left: 10.0, bottom: 10),
+              child: OutlinedButton(
+                  onPressed: _launchURL,
+                  child: Text(
+                    "Illustrations powered by Storyset",
+                    style: TextStyle(fontWeight: FontWeight.w300),
+                  )),
+            )
           ],
         ),
       ),
     );
+  }
+
+  void _launchURL() async {
+    if (!await launch("https://storyset.com/")) throw 'Could not launch';
   }
 }
